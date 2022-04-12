79af208ab9c886a9355c601135dcc25a24954af7
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-04-12 17:38:39 -0400

    [proj04] Makefile separated student/hashtable

--- proj04-comparative_benchmark/hashtable.cpp
+++ proj04-comparative_benchmark/hashtable.cpp
@@ -2,23 +2,6 @@
 #include<vector>
 using namespace std;
 
-class student{
-    public:
-    string first_name;
-    string last_name;
-    int zip_code;
-    float gpa;
-    student(string f, string l, int z, float g){
-        first_name = f;
-        last_name = l;
-        zip_code = z;
-        gpa = g;
-    }
-    void display(){
-        cout << first_name << " " << last_name << " " << zip_code << " " << gpa << endl;
-    }
-};
-
 class hash_table{
 
     public:
