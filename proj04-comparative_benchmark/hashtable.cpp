6ea1b50b3f1799279244249242d53064e7889834
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-04-12 17:48:38 -0400

    [proj04] header for student

--- proj04-comparative_benchmark/hashtable.cpp
+++ proj04-comparative_benchmark/hashtable.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include"student.h"
 using namespace std;
 
 class hash_table{
--- proj04-comparative_benchmark/student.h
+++ proj04-comparative_benchmark/student.h
@@ -1,16 +1,20 @@
-// student.cpp
-#include"student.h"
 #include<iostream>
 using namespace std;
 
-student::student(string f, string l, int z, float g){
+class student{
+    public:
+    string first_name;
+    string last_name;
+    int zip_code;
+    float gpa;
+    student(string f, string l, int z, float g){
         first_name = f;
         last_name = l;
         zip_code = z;
         gpa = g;
-}
-
-void student::display(){
+    }
+    void display(){
         cout << first_name << " " << last_name << " " << zip_code << " " << gpa << endl;
-}
+    }
+};
 
